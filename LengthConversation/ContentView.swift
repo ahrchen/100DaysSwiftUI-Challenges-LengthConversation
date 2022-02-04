89b102331faaf11d11c7aecf5cfe14c805255b89
Raymond Chen <ahrchen@gmail.com>
2022-02-03 21:08:17 -0800

    Initial Commit

---
+++ LengthConversation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LengthConversation
+//
+//  Created by Raymond Chen on 2/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LengthConversation/LengthConversationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConversationApp.swift
+//  LengthConversation
+//
+//  Created by Raymond Chen on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct LengthConversationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
