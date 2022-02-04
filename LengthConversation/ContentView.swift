dfc3a84b5b5c6f599998fc727ccd964f65cd696a
Raymond Chen <ahrchen@gmail.com>
2022-02-03 22:29:28 -0800

    Add Input and Output Length

--- LengthConversation/ContentView.swift
+++ LengthConversation/ContentView.swift
@@ -8,14 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var convertUnit: UnitLength = .meters
+    @State private var toUnit: UnitLength = .feet
+    @State private var convertLength: Double = 0.0
+    
+    private var toLength: Measurement<UnitLength> {
+        let inputValue = Measurement(value: convertLength, unit: convertUnit)
+        let outputValue = inputValue.converted(to: toUnit)
+        return outputValue
+    }
+    
+    let units: [UnitLength] = [.meters, .kilometers, .feet, .yards, .miles]
+    let formatter: MeasurementFormatter
+    
+    init() {
+        formatter = MeasurementFormatter()
+        formatter.unitOptions = .providedUnit
+        formatter.unitStyle = .medium
+    }
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Convert Length", selection: $convertUnit) {
+                        ForEach(units, id: \.self) { unit in
+                            Text(unit.localizedName)
+                        }
+                    }
+                    TextField("Input", value: $convertLength, format: .number)
+                }
+                
+                Section {
+                    Picker("To Length", selection: $toUnit) {
+                        ForEach(units, id: \.self) { unit in
+                            Text(unit.localizedName)
+                        }
+                    }
+                    Text(toLength, formatter: formatter)
+                }
+            }
+            .navigationTitle("Length Conversion")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension UnitLength {
+    var localizedName: String {
+        if self == UnitLength.meters {
+            return "Meter"
+        } else if self == UnitLength.kilometers {
+            return "Kilometer"
+        } else if self == UnitLength.feet {
+            return "Feet"
+        } else if self == UnitLength.yards {
+            return "Yards"
+        } else if self == UnitLength.miles {
+            return "Miles"
+        } else {
+            return "Unknown"
+        }
     }
 }
